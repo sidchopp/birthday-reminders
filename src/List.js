06ab64184395a80b7bf039296c6cfaf4f12756bd
Sid <sidchopp@gmail.com>
2021-08-24 13:32:11 -0600

    slight css changes

--- src/List.js
+++ src/List.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, Button, Figure, Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaFacebookSquare } from 'react-icons/fa';
 
 
 import data from './data'
@@ -13,23 +13,23 @@ const List = ({ people }) => {
       {people.map((person) => {
         const { id, name, birthday, image, about } = person;
         return (
-          <Card className="text-center" key={id} >
-            <Card.Header>{person.name}</Card.Header>
-            <Figure>
+          <Card className="text-center  " key={id} >
+            <Card.Header className="bg-dark text-white">{person.name}</Card.Header>
+            {/* <Figure >
               <Figure.Image
                 width={1500}
                 height={800}
                 src={image} alt={name}
-              /></Figure>
+              /></Figure> */}
             {/* <Image variant="top" src={image} alt={name} /> */}
 
-            {/* <Card.Img variant="top" src={image} alt={name} /> */}
+            <Card.Img variant="top" src={image} alt={name} />
             <Card.Body>
               <Card.Title>{name}</Card.Title>
               <Card.Text>
                 {birthday}
               </Card.Text>
-              <Button variant="primary" as='a' href={about} target="_blank" > <FaFacebookF /></Button>
+              <Button variant="primary" as='a' href={about} target="_blank" > <FaFacebookSquare /></Button>
             </Card.Body>
           </Card>
         );
